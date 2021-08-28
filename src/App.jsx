245a8bd36bc7cce248b343c394e4d76fe2fb569f
ravishukla86044 <ravishukls86044@gmail.com>
2021-08-28 14:06:00 +0530

    merging Amit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import "./App.css";
-<<<<<<< HEAD
 import { CartPage } from "./Components/CartPage/CartPage";
 import { Switch, Route } from "react-router-dom";
 import HomeSkyPg from "./Components/HomePage/Home_skyscanner";
 import { useRef } from "react";
 import { HotelDescription } from "./Components/HotelDescription/HotelDescription";
-=======
-//import { CartPage } from "./Components/CartPage/CartPage";
-import MainPaymentPage from "./Components/Payment-page/MainPaymentPage"
->>>>>>> 16861a095511ec044734e3e2a0c3c898ac994ae5
 function App() {
   const formData = useRef();
 
@@ -27,10 +22,6 @@ function App() {
         </Route>
       </Switch>
       <CartPage />
-=======
-      {/* <CartPage /> */}
-      <MainPaymentPage/>
->>>>>>> 16861a095511ec044734e3e2a0c3c898ac994ae5
     </div>
   );
 }
