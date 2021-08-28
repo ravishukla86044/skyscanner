a97102733f1d27ff368f12623164ffe2da609172
tauseef <tauseef@gmail>
2021-08-28 13:27:59 +0530

    3rd page2

--- src/Components/HotelDescription/HotelDescription.jsx
+++ src/Components/HotelDescription/HotelDescription.jsx
@@ -15,6 +15,18 @@ import PoolRoundedIcon from '@material-ui/icons/PoolRounded';
 import FitnessCenterRoundedIcon from '@material-ui/icons/FitnessCenterRounded';
 import MeetingRoomRoundedIcon from '@material-ui/icons/MeetingRoomRounded';
 import ArrowForwardRoundedIcon from '@material-ui/icons/ArrowForwardRounded';
+import LocalCafeRoundedIcon from '@material-ui/icons/LocalCafeRounded';
+import LocalBarRoundedIcon from '@material-ui/icons/LocalBarRounded';
+import LocalDrinkRoundedIcon from '@material-ui/icons/LocalDrinkRounded';
+import RestaurantRoundedIcon from '@material-ui/icons/RestaurantRounded';
+import ConfirmationNumberRoundedIcon from '@material-ui/icons/ConfirmationNumberRounded';
+import SmokeFreeRoundedIcon from '@material-ui/icons/SmokeFreeRounded';
+import LocalLaundryServiceRoundedIcon from '@material-ui/icons/LocalLaundryServiceRounded';
+import TableChartRoundedIcon from '@material-ui/icons/TableChartRounded';
+import BathtubRoundedIcon from '@material-ui/icons/BathtubRounded';
+import NoteRoundedIcon from '@material-ui/icons/NoteRounded';
+import AirlineSeatFlatAngledRoundedIcon from '@material-ui/icons/AirlineSeatFlatAngledRounded';
+import AirlineSeatFlatRoundedIcon from '@material-ui/icons/AirlineSeatFlatRounded';
 import { dataa } from "../CartPage/utils/dataa";
 export const HotelDescription = () => {
     
@@ -31,6 +43,11 @@ export const HotelDescription = () => {
                     return <div className={`ttopItem${i+1}`} > <img src={item} alt=""/></div>
                 })}
             </div>
+              
+            <div className="adviser" style={{marginLeft:"10%"}}>
+            <div className="SectionHeader_SectionHeader__1YzyH"><h1 className="BpkText_bpk-text__1KRVP BpkText_bpk-text--xxxl__1QpyP SectionHeader_SectionHeader__title__2lkEs"><img alt="TripAdvisor" height="47" className="ReviewsFromTA_ReviewsFromTA__image__1-ngD" src="https://www.tripadvisor.com/img/cdsi/img2/branding/v2/Tripadvisor_lockup_horizontal_secondary_registered-64600-0.svg"/> reviews</h1></div>
+             </div>
+
             <div className="tcheckIn1">
             <div className="SectionHeader_SectionHeader__1YzyH"><h3 className="BpkText_bpk-text__1KRVP BpkText_bpk-text--xxxl__1QpyP SectionHeader_SectionHeader__title__2lkEs">Check in and check out</h3></div>
             </div>
@@ -112,7 +129,7 @@ export const HotelDescription = () => {
                     <h3>Accepted payment methods</h3>
                     </div>
                     <div  className="tcardstypes"  >
-                    <div class="PaymentCardPolicy_PaymentCardPolicy__cardLogo__3JaB0"><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/master-card.de882b11.svg" alt="card logo"/><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/visa.62f951bc.svg" alt="card logo"/><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/amex.6be00169.svg" alt="card logo"/><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/diners.90c82454.svg" alt="card logo"/><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/jcb.9dbc7265.svg" alt="card logo"/><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/discover.51d7c5b8.svg" alt="card logo"/><section></section></div>
+                    <div className="PaymentCardPolicy_PaymentCardPolicy__cardLogo__3JaB0"><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/master-card.de882b11.svg" alt="card logo"/><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/visa.62f951bc.svg" alt="card logo"/><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/amex.6be00169.svg" alt="card logo"/><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/diners.90c82454.svg" alt="card logo"/><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/jcb.9dbc7265.svg" alt="card logo"/><img src="https://js.skyscnr.com/sttc/hotels-website/hotels-website/static/media/discover.51d7c5b8.svg" alt="card logo"/><section></section></div>
                     </div>
                 </div>
                 
@@ -170,7 +187,8 @@ export const HotelDescription = () => {
                 </div>
             </div>
 
-            <div className="tamenities2">
+               <div className="biggeranemities">
+               <div className="tamenities2">
                 <div>
                    <h3>Buisiness Facilities</h3>
                 </div>
@@ -191,12 +209,372 @@ export const HotelDescription = () => {
                     <div style={{marginLeft:"3px"}}>
                     <p>Meeting Facilities</p>
                     </div>
-                </div>
+                    </div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Theater Facilities</p>
+                    </div>
+                    </div>
+                    
                    
+                </div>
+                <hr style={{width:"70%",margin:"auto"}}></hr>
+                
+                <div className="tamenities2">
                 <div>
+                   <h3>Food and drink</h3>
+                </div>
+                       
+                    <div>
+                        
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <LocalCafeRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Cafe</p>
+                    </div>
+                    </div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <LocalBarRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Bar</p>
+                    </div>
+                    </div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Banqueting service</p>
+                    </div>
+                    </div> 
+
+                    </div>
+
+                    <div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <LocalDrinkRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Water Bottle</p>
+                    </div>
+                    </div>
+                        
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <RestaurantRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Restaurent</p>
+                    </div>
+                    </div>
+                        
+                        <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <LocalCafeRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Coffe Maker</p>
+                    </div>
+                    </div> 
+                        
+                    </div>
+
+                    <div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Breakfast Room</p>
+                    </div>
+                    </div>
+                        
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Barbecue</p>
+                    </div>
+                    </div> 
+
+                    </div>
+                    
                     
                 </div>
-            </div>
+
+                <hr style={{width:"70%",margin:"auto"}}></hr>
+
+                <div className="tamenities2">
+                <div>
+                   <h3>Outdoor facility</h3>
+                </div>
+                       
+                    <div>
+                        
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Shops</p>
+                    </div>
+                    </div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ConfirmationNumberRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Ticket Office</p>
+                    </div>
+                    </div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Wedding Facilities</p>
+                    </div>
+                    </div> 
+
+                    </div>
+
+                    <div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Smoking Areas</p>
+                    </div>
+                    </div>
+                        
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <SmokeFreeRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Non Smoking Floor</p>
+                    </div>
+                    </div>
+                        
+                        <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Lounge</p>
+                    </div>
+                    </div> 
+                        
+                    </div>
+
+                    <div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <SmokeFreeRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Non Smoking</p>
+                    </div>
+                    </div>
+                        
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <LocalLaundryServiceRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Laundry</p>
+                    </div>
+                    </div> 
+
+                    </div>
+                    
+                </div>
+                 <hr style={{width:"70%",margin:"auto"}}></hr>
+                <div className="tamenities2">
+                <div>
+                   <h3>In room</h3>
+                </div>
+                       
+                    <div>
+                        
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <TableChartRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Desk</p>
+                    </div>
+                    </div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Hair Dryer</p>
+                    </div>
+                    </div>
+
+                   
+                    </div>
+
+                    <div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <AcUnitRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Air Condition</p>
+                    </div>
+                    </div>
+                        
+                   
+                        
+                    </div>
+
+                    <div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <BathtubRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Shower</p>
+                    </div>
+                    </div>
+                        
+                   
+
+                    </div>
+                    
+                    
+                </div>
+
+                <hr style={{width:"70%",margin:"auto"}}></hr>
+                <div className="tamenities2">
+                <div>
+                   <h3>Leisure</h3>
+                </div>
+                       
+                    <div>
+                        
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <BathtubRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Suana</p>
+                    </div>
+                    </div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <NoteRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Newspaper</p>
+                    </div>
+                    </div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Library</p>
+                    </div>
+                    </div> 
+
+                    </div>
+
+                    <div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <AirlineSeatFlatAngledRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Massage</p>
+                    </div>
+                    </div>
+                        
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Steam Room</p>
+                    </div>
+                    </div>
+                        
+                        <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Salon</p>
+                    </div>
+                    </div> 
+                        
+                    </div>
+
+                    <div>
+
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Pool Bar</p>
+                    </div>
+                    </div>
+                        
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <ArrowForwardRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Night Club</p>
+                    </div>
+                    </div> 
+                    
+                    <div style={{display:"flex"}}>
+                    <div style={{marginTop:"16px"}}>
+                    <AirlineSeatFlatRoundedIcon fontSize="small" />
+                    </div>
+                    <div style={{marginLeft:"3px"}}>
+                    <p>Spa</p>
+                    </div>
+                    </div>
+                    </div>
+                    
+                    
+                </div>
+                 
+                 
+
+         </div>
+           
           
     </div>
     )
